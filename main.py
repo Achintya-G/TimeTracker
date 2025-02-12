02acef0a3382b9a82bcc0683a6be3244bbd361a8
achin <achintya.g.available@gmail.com>
2025-02-12 12:59:25 +0530

    Created function to track when focused window is changed

--- main.py
+++ main.py
@@ -4,15 +4,41 @@ import time
 import pywinctl
 
 
+def trackWindowChanges():
+    """Continuously monitors for active window changes."""
+    last_window = None
+
+
+    while True:
+        active_window = pywinctl.getActiveWindow()
+        active_title = active_window.title if active_window else "No Active Window"
+
+        if active_title != last_window and active_title is not None:
+            print("Window changed to:", active_title)
+            onWindowChange(active_window.getAppName(),datetime.datetime.now())
+            last_window = active_title
+        
+
+        time.sleep(1)  
+
+
+def onWindowChange(appName,timestamp):
+    print(appName)
+    pass
+
+
+def active():
+    global window
+    print("ac called")
+    global windowWatcher
+    print(window.getAppName())# to get focused app name
+    windowWatcher.stop()
+
 def dbconn():
     conn = sqlite3.connect("time_tracker.db",)
     cur = conn.cursor()
     conn.close()
 
-d = pywinctl.getAllAppsWindowsTitles()  # to get all applications open
-for i in d:
-    print(i, len(d[i]))
 
-while True:
-    time.sleep(1)
-    print(pywinctl.getActiveWindow().getAppName())  # to get focused app name
+# Start tracking
+trackWindowChanges()
